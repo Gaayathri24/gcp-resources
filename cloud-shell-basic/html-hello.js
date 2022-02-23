2a70a9ed5b27e3cf60bb4890bd6a680a595cc6be
Gaayathri24 <57406264+Gaayathri24@users.noreply.github.com>
2022-02-23 15:16:35 +0000

    Update html-hello.js

--- cloud-shell-basic/html-hello.js
+++ cloud-shell-basic/html-hello.js
@@ -3,7 +3,7 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8081;
 
-const message = 'This is G3's sandbox using CloudShell to display a html webpage #prep #GCP #interview';
+const message = 'This is my sandbox trying CloudShell to display html web content on 8081 #GCPInterview';
 const fullMessage = `<html><body><h1>${message}</h1></body></html>\n`;
 
 const server = http.createServer((request, response) => {
